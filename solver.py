05685a151506fb133dfbf2ccd672ad86b8643b54
Carli* Freudenberg <kound@posteo.de>
2021-03-05 14:46:14 +0100

    Testing Version 1

--- solver.py
+++ solver.py
@@ -43,7 +43,7 @@ def get_cube_numbers(array: List[List[int]],row,col):
         if not np.isnan(array[i,j])
     }
 
-input = np.array([
+input_matrix = np.array([
     [n, 3, n,  n, n, n,  n, n, n],
     [n, n, n,  1, 9, 5,  n, n, n],
     [n, n, 8,  n, n, n,  n, n, 6],
@@ -57,6 +57,29 @@ input = np.array([
     np.float16
 )
 
+solution_matrix = np.array([
+    [5, 3, 4,  6, 7, 8,  9, 1, 2],
+    [6, 7, 2,  1, 9, 5,  3, 4, 8],
+    [1, 9, 8,  3, 4, 2,  5, 6, 7],
+    [8, 5, 9,  7, 6, 1,  4, 2, 3],
+    [4, 2, 6,  8, 5, 3,  7, 9, 1],
+    [7, 1, 3,  9, 2, 4,  8, 5, 6],
+    [9, 6, 1,  5, 3, 7,  2, 8, 4],
+    [2, 8, 7,  4, 1, 9,  6, 3, 5],
+    [3, 4, 5,  2, 8, 6,  1, 7, 9],
+])
+
+test1 = np.array([
+    [5, 3, n,  n, 7, n,  n, n, n],
+    [6, n, n,  1, 9, 5,  n, n, n],
+    [n, 9, 8,  3, 4, 2,  5, 6, 7],
+    [8, n, n,  n, 6, n,  n, n, 3],
+    [4, n, n,  8, n, 3,  n, n, 1],
+    [7, n, n,  n, 2, n,  n, n, 6],
+    [n, 6, n,  n, n, n,  2, 8, n],
+    [n, n, n,  4, 1, 9,  n, n, 5],
+    [n, n, n,  n, 8, n,  n, 7, 9],
+])
 
 def get_possibilities(matrix: List[List[int]], row: int, col: int) -> Set[int]:
     possible = base_possibilities.copy()
@@ -109,4 +132,4 @@ def solver(matrix: List[List[int]], possibs_matrix: Dict[Tuple[int, int], Set[in
             pass
     raise SolutionNotFound("In the Main Solver no solution was found")
 
-print_sodouk(solver(input))
+print_sodouk(solver(test1))
